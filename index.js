e5b1da7bb4a43891a37043f31ff2bdc69995a252
srkvodnala <srkrao547@gmail.com>
2020-03-24 23:37:17 -0500

    heroku

--- index.js
+++ index.js
@@ -2,15 +2,21 @@ const http = require('http');
 
 
 const hostname = process.env.hostname || '0.0.0.0'
-const port = process.env.PORT || 8080
+    // const port = process.env.PORT || 3003
 
 
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World');
+    res.end('Hello World - Wash your hands');
 });
 
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+    port = 3011;
+}
+server.listen(port, () => {
+    console.log("Server running on port 3011");
 });
